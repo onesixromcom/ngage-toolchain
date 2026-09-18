df1ab33d48b10c025ba45af27334d45a94c89570
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2026-09-18 15:10:10 +0300

    Fix build for triangle project

--- projects/triangle/build.bat
+++ projects/triangle/build.bat
@@ -6,7 +6,7 @@ rd /s /q %BUILD_FODLER%
 
 cmake -G"Unix Makefiles" -Wno-dev -DCMAKE_MAKE_PROGRAM="c:\Program Files (x86)\GnuWin32\bin\make.exe" -DCMAKE_TOOLCHAIN_FILE="%NGAGESDK%\cmake\ngage-toolchain.cmake" -B %BUILD_FODLER%
 
-#cmake --build %BUILD_FODLER% --target launcher
+cmake --build %BUILD_FODLER% --target launcher
 cmake --build %BUILD_FODLER% --target launcher_app_target
 cmake --build %BUILD_FODLER% --target launcher.rsc
 cmake --build %BUILD_FODLER% --target launcher.aif
