5f9ba0151105c55fffca49427f6c623b4cba06be
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2026-09-18 15:06:32 +0300

    Fix build for triangle project

--- projects/triangle/README.md
+++ projects/triangle/README.md
@@ -1,3 +1,15 @@
 # Triangle
 
 3D spinning triangle for the Nokia N-Gage.
+
+## Build
+
+Be sure you all of these apps installed.
+
+- cmake
+- cygwin
+- git
+- strawberry perl
+- Python 3.9.12
+
+Use build.bat to build all and output ready to use triangle_app.sis file.
---
+++ projects/triangle/build.bat
@@ -0,0 +1,14 @@
+@setlocal
+
+set BUILD_FODLER="out/build/N-Gage"
+
+rd /s /q %BUILD_FODLER%
+
+cmake -G"Unix Makefiles" -Wno-dev -DCMAKE_MAKE_PROGRAM="c:\Program Files (x86)\GnuWin32\bin\make.exe" -DCMAKE_TOOLCHAIN_FILE="%NGAGESDK%\cmake\ngage-toolchain.cmake" -B %BUILD_FODLER%
+
+#cmake --build %BUILD_FODLER% --target launcher
+cmake --build %BUILD_FODLER% --target launcher_app_target
+cmake --build %BUILD_FODLER% --target launcher.rsc
+cmake --build %BUILD_FODLER% --target launcher.aif
+cmake --build %BUILD_FODLER% --target triangle
+cmake --build %BUILD_FODLER% --target launcher.sis
