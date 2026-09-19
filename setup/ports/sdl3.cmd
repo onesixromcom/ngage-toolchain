b5d31ed3642905452b170aedc252bc7f8a4fc9d7
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2026-09-19 15:49:35 +0300

    Added command to build libSDL3debug.a lib

--- setup/ports/sdl3.cmd
+++ setup/ports/sdl3.cmd
@@ -6,6 +6,8 @@ git clone https://github.com/libsdl-org/SDL.git --depth 1 --single-branch build/
 
 cd build/SDL3
 
+# Build libSDL3.a
+
 rd /s /q %BUILD_FODLER% && mkdir %BUILD_FODLER%
 
 cmake -G"Unix Makefiles" -Wno-dev ^
@@ -13,6 +15,24 @@ cmake -G"Unix Makefiles" -Wno-dev ^
   -DCMAKE_TOOLCHAIN_FILE=%NGAGESDK%/cmake/ngage-toolchain-sdl3.cmake ^
   -DBUILD_SHARED_LIBS=OFF ^
   -DCMAKE_BUILD_TYPE=Release ^
+  -DSDL_STATIC=ON ^
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
+  -DSDL_TESTS=OFF -DSDL_TEST_LIBRARY=OFF -DSDL_INSTALL_TESTS=OFF ^
+  -DCMAKE_INSTALL_PREFIX="%NGAGESDK%/sdk/extras" ^
+  -B %BUILD_FODLER%
+  
+cmake --build %BUILD_FODLER% --config Release
+cmake --install %BUILD_FODLER%
+
+# Build libSDL3debug.a
+
+rd /s /q %BUILD_FODLER% && mkdir %BUILD_FODLER%
+
+cmake -G"Unix Makefiles" -Wno-dev ^
+  -DCMAKE_MAKE_PROGRAM="c:\Program Files (x86)\GnuWin32\bin\make.exe" ^
+  -DCMAKE_TOOLCHAIN_FILE=%NGAGESDK%/cmake/ngage-toolchain-sdl3.cmake ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DCMAKE_BUILD_TYPE=Debug ^
   -DSDL_STATIC=ON ^
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
   -DSDL_TESTS=OFF -DSDL_TEST_LIBRARY=OFF -DSDL_INSTALL_TESTS=OFF ^
